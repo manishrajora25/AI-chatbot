9a84cf7a01c8235662549c94d0364bf1dacd6739
manishrajora25 <manishrajora453@gmail.com>
2025-12-10 14:36:04 +0530

    bbn

--- Frontend/vite-project/src/pages/Register.jsx
+++ Frontend/vite-project/src/pages/Register.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { useNavigate, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import axiosInstance from "../config/axiosConfig";
 
 
