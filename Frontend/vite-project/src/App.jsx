8c4cb29eaaf22723183d91de2684e94caccd295d
manishrajora25 <manishrajora453@gmail.com>
2025-12-10 13:06:12 +0530

    cncnc

--- Frontend/vite-project/src/App.jsx
+++ Frontend/vite-project/src/App.jsx
@@ -16,7 +16,9 @@
 
 
 
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Chat from "./pages/Chat";
