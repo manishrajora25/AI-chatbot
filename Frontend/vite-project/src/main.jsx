46b74a302c886174152148baba523464d305e207
manishrajora25 <manishrajora453@gmail.com>
2026-01-02 13:20:31 +0530

    vhjk

--- Frontend/vite-project/src/main.jsx
+++ Frontend/vite-project/src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+console.log("ENV API =", import.meta.env.VITE_API_URL);
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
